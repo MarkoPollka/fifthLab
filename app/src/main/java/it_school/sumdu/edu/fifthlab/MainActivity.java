c2bd18ccac87543637478a9f72190d2d934809da
Vlad Savchenko <polkamaraka@gmail.com>
2020-04-30 13:17:31 +0300

    Initial commit

---
+++ app/src/main/java/it_school/sumdu/edu/fifthlab/MainActivity.java
@@ -0,0 +1,25 @@
+package it_school.sumdu.edu.fifthlab;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+public class MainActivity extends AppCompatActivity {
+    List<Phone> phones = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setInitialData();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+        // создаем адаптер
+        DataAdapter adapter = new DataAdapter(this, phones);
+        // устанавливаем для списка адаптер
+        recyclerView.setAdapter(adapter);
+    }
+    private void setInitialData(){
+        phones.add(new Phone ("Huawei P10", "Huawei", R.drawable.mate8));
+        phones.add(new Phone ("Elite z3", "HP", R.drawable.lumia950));
+        phones.add(new Phone ("Galaxy S8", "Samsung", R.drawable.galaxys6));
+        phones.add(new Phone ("LG G 5", "LG", R.drawable.nexus5x));
+    }
+}
